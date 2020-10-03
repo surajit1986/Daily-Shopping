4098b82a585076a1a9641095a1b683cb9e19702d
surajit1986 <surajit.1986@gmail.com>
2020-10-04 00:16:18 +0530

    Prodct, type and brand are added

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -4,6 +4,7 @@ using Infrastructure.Data;
 using Core.Entities;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Core.Interfaces;
 
 namespace API.Controllers
 {
@@ -11,24 +12,25 @@ namespace API.Controllers
     [Route("api/[controller]")]
     public class ProductsController : ControllerBase
     {
-        private readonly StoreContext _context;
-        public ProductsController(StoreContext context)
-        {
-            _context = context;
+        private readonly IProductRepository _repo;
 
+        public ProductsController(IProductRepository repo)
+        {
+            _repo = repo;
         }
 
         [HttpGet]
         public async Task<ActionResult<IEnumerable<Product>>> GetProducts()
         {
-            return await _context.Products.ToListAsync();
+            var products =  await _repo.getProductsAsync();
+            return Ok(products);
 
         }
 
         [HttpGet("{id}")]
         public async Task<ActionResult<Product>> Getproduct(int id)
         {
-            return await _context.Products.FindAsync(id);
+            return await _repo.getproductByIdAsync(id);
 
         }
 
